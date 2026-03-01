38e5926f02293ab036abab3046357f17c59efa56
Hrishikesh Nadgouda <81570589+Oofokboomeryeetmaster3000@users.noreply.github.com>
2026-02-28 19:51:20 -0500

    Add files via upload

---
+++ subsystems.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+inline pros::MotorGroup intake ({-1,10});
+inline ez::Piston MatchLoader('A');
+inline ez::Piston Descore('B');
+
+inline ez::Piston RedirectT('D');
+inline ez::Piston RedirectB('E');
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
